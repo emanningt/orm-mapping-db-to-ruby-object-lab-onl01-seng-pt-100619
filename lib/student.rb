49c98ae902fee3219d01c882676c5b3098474e26
Eric Manning <emanningmusic@yahoo.com>
2020-01-07 23:03:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,8 +72,10 @@ class Student
   end
   
   def self.students_below_12th_grade
-    sql = "SELECT * FROM students
-    WHERE grade > 12;"
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade > 12;
+    SQL
     DB[:conn].execute(sql)
   end 
   
